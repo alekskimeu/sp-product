45d172d90f6430eb9e78952cc7c7d913fd555dcb
alekskimeu <kimeualeks@gmail.com>
2025-02-14 06:07:47 +0300

    write unit test for creating a product

--- src/test/java/com/product/sp_product/SpProductApplicationTests.java
+++ src/test/java/com/product/sp_product/SpProductApplicationTests.java
@@ -1,15 +1,53 @@
 package com.product.sp_product;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpProductApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+							{
+				    "name": "Motorolla Flap",
+				    "description": "You can never go wrong with a Motorolla",
+				    "price": 1500
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/v1/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("Motorolla Flap"))
+				.body("description", Matchers.equalTo("You can never go wrong with a Motorolla"))
+				.body("price", Matchers.equalTo(1500));
 	}
 
 }
